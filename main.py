028e09e86a889b0477ed3bb58b55d473d51ef19e
hannahhall <hannahkhall13@gmail.com>
2022-04-13 11:59:07 -0500

    initial functions

---
+++ main.py
@@ -0,0 +1,20 @@
+# Accept user input for github org, slack channel, and pat
+
+def create_repo_from_template():
+    pass
+
+def deploy_site():
+    pass
+
+def post_to_slack():
+    pass
+
+def main():
+    template_url = "https://github.com/nashville-software-school/Class-Website"
+    github_org = input("What is the github organization?")
+    slack_channel = input("What is the cohort's slack channel?")
+    pat = input("What is your access token?")
+
+
+if __name__ == "__main__":
+    main()
